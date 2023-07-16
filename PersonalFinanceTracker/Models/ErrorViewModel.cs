4ae23c570481796dd624e80fb8d30fb1d3b84d5e
zcienka <zuzannacienka@gmail.com>
2023-07-16 21:58:40 +0200

    Initial commit

---
+++ PersonalFinanceTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalFinanceTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
